9e9b8f87b7f8aadf389bdc22e99ed5106aa8d53c
Nikolai Frank Nilsen <franknilsen9@gmail.com>
2020-05-14 10:35:02 -0700

    Prelim hook up application/managers to main activity

--- app/src/main/java/com/example/httpjsonparser/MainActivity.kt
+++ app/src/main/java/com/example/httpjsonparser/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.httpjsonparser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var httpManager: HttpManager
+    lateinit var likesManager: LikesManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        httpManager = (application as JsonParserApp).httpManager
+        likesManager = (application as JsonParserApp).likesManager
+
+        fetch_button.setOnClickListener{
+            fetchUserInfo()
+        }
+    }
+
+    private fun fetchUserInfo() {
+        httpManager.getUserInfo (
+            { userInfo ->
+                toastMessage(userInfo.username)
+            },
+            { statusCode ->
+                toastMessage("Error! Status Code $statusCode")
+            }
+        )
+    }
+
+    private fun toastMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
